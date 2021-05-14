16719e1cecd86ede94317103c8346bc379662be9
Daniel Carmona <carmona240101@hotmail.com>
2021-05-14 10:08:57 +0100

    criada a classe para aceder à tabela de livros

--- app/src/main/java/pt/ipg/livros2/TabelaLivros.kt
+++ app/src/main/java/pt/ipg/livros2/TabelaLivros.kt
@@ -1,4 +1,55 @@
 package pt.ipg.livros2
 
-class TabelaLivros {
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaLivros (db: SQLiteDatabase) : BaseColumns {
+    private val db : SQLiteDatabase = db
+    fun cria() {
+        db?.execSQL(
+                "CREATE TABLE " + NOME_TABELA + "(" +
+                        BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        CAMPO_TITULO + " TEXT NOT NULL," +
+                        CAMPO_AUTOR + " TEXT NOT NULL," +
+                        CAMPO_ID_CATEGORIA + "INTEGER NOT NULL," +
+                        "FOREIGN KEY (" + CAMPO_ID_CATEGORIA + ")" +
+                        "REFERENCES" + TabelaCategorias.NOME_TABELA +
+                        ")"
+        )
+
+    }
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(NOME_TABELA, null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_TABELA, whereClause, whereArgs)
+    }
+
+    fun query(
+            columns: Array<String>,
+            selection: String,
+            selecyionArgs: Array<String>,
+            groupBy: String, having: String,
+            orderBy: String
+    ): Cursor? {
+        return db.query(NOME_TABELA, columns, selection, selecyionArgs, groupBy, having, orderBy)
+    }
+
+    companion object{
+        const val NOME_TABELA = "livros"
+        const val CAMPO_TITULO = "titulo"
+        const val CAMPO_AUTOR = "autor"
+        const val CAMPO_ID_CATEGORIA = "id_categoria"
+        //categoria         livros
+        //     1   -------    N
+    }
+
 }
