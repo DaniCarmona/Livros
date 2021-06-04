7a4439cd01a2bc34eb6f7dbf9311d45f6a3d972c
Daniel Carmona <carmona240101@hotmail.com>
2021-06-04 11:02:03 +0100

    passar os dados dos livros para o adptador (RecyclerView)

--- app/src/main/java/pt/ipg/livros2/AdapterLivros.kt
+++ app/src/main/java/pt/ipg/livros2/AdapterLivros.kt
@@ -1,7 +1,10 @@
 package pt.ipg.livros2
 
+import android.database.Cursor
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterLivros : RecyclerView.Adapter<AdapterLivros.ViewHolderLivro>  {
-    class ViewHolderLivro(itemView : View ) : RecyclerView.ViewHolder
+class AdapterLivros(var cursor : Cursor? = null) : RecyclerView.Adapter<AdapterLivros.ViewHolderLivro>  {
+    class ViewHolderLivro(itemView : View ) : RecyclerView.ViewHolder{
+
+    }
 }
